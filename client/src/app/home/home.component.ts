1d01ea36e9bfe1ac367699258386479d3b56e55d
Supattalak-Phoha <supattalak.p@gmail.com>
2024-08-08 23:33:06 +0700

    POC Css

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../data.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,17 @@ import { DataService } from '../data.service';
   styleUrls: ['./home.component.scss', '../../../public/assets/css/styles.scss']
 })
 export class HomeComponent {
+  content = {
+    text01: " บริการรับจดเค",
+    text02: "บริการรับจดเครื่องหมายการค้า บริการขอใบ "
+  }
   data: any[] = [];
   isImageLoaded01 = false;
   isImageLoaded02 = false;
 
-  constructor(private dataService: DataService) { }
+
+  constructor(private dataService: DataService) {
+   }
 
   onImageLoad(picture: string) {
     if (picture === 'isImageLoaded01') {
