ecf75b651eccbfc1308dce3908ba29fb7d71d845
Supattalak-Phoha <supattalak.p@gmail.com>
2024-08-14 23:19:13 +0700

    Update Code

--- client/src/app/services/all-services/all-services.component.ts
+++ client/src/app/services/all-services/all-services.component.ts
@@ -6,59 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-services.component.scss', './../../../styles.scss']
 })
 export class AllServicesComponent {
-
+  contents = {
+    content001: "บริการของเรา",
+    content002: "บริษัทของเรามีบริการต่าง ๆ มากมาย เพื่อตอบสนองความต้องการของคุณ"
+  }
 
   arrays = {
     array001: [
       {
-        name: "รับจดเครื่องหมายการค้า",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "อาหารและยา (อย.)",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-001.jpg"
       },
       {
-        name: "รับจดสิทธิบัตร อนุสิทธิบัตร",
-        description: "บริการรับจดทะเบียนสิทธิบัตร สิทธิบัตรการประดิษฐ์ อนุสิทธิบัตร และสิทธิบัตรการออกแบบผลิตภัณฑ์",
-        image001: "assets/images/home/home-002.jpg"
+        name: "มาตรฐานผลิตภัณฑ์อุตสาหกรรม (มอก.)",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-002.jpg"
       },
       {
-        name: "รับจดลิขสิทธิ์",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "บัญชีนวัตกรรมไทย",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-003.jpg"
       },
       {
-        name: "รับจดเครื่องหมายการค้า",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "เครื่องหมายการค้า ลิขสิทธิ์ และจดทะเบียนต่าง ๆ",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-004.jpg"
       },
       {
-        name: "รับว่าความ รับจ้างเป็นทนายความ",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "บาร์โค้ด",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-005.jpg"
+      },
+
+      {
+        name: "ส่งเสริมการลงทุน (BOI)",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-006.jpg"
       },
       {
-        name: "รับจดเครื่องหมายการค้าจีน เครื่องหมายการค้าต่างประเทศ",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "Accounting and Tax",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-007.jpg"
       },
       {
-        name: "รับจดสิทธิบัตรต่างประเทศ",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "GMP HACCP",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-008.jpg"
       },
       {
-        name: "รับขึ้นทะเบียนและจดบัญชีนวัตกรรมไทย",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "รับรองฮาลาล (HALAL)",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-009.jpg"
       },
       {
-        name: "รับจดทะเบียนและขอใบอนุญาต มอก. สมอ.",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "VISA and Work permit",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-010.jpg"
       },
       {
-        name: "รับจดทะเบียนและขอใบอนุญาต อย.",
-        description: "บริการรับจดทะเบียนเครื่องหมายการค้า เครื่องหมายร่วม จดลิขสิทธิ์โลโก้ จดแบรนด์ ในประเทศไทย",
-        image001: "assets/images/home/home-002.jpg"
+        name: "ขอรับรอง Made in Thailand (MiT)",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-011.jpg"
+      },
+      {
+        name: "ปรึกษากฏหมาย",
+        description: "รอดำเนินการ...",
+        image001: "assets/images/services/service-012.jpg"
       }
     ]
   }
